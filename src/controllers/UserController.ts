38c0c164b7e45ccedacdcdc6b2d2ab4a4bf4e4b3
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-03-29 20:58:06 -0300

    Removes unused method

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,8 +4,4 @@ export class UserController {
   public async getAllUsers(_req: Request, res: Response) {
     res.status(200).json({ users: [] });
   }
-
-  public async createNewUser(req: Request, res: Response) {
-    const { username, email, password } = req.body;
-  }
 }
