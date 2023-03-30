8c3f6d2c4f55d04f9f2a767ae6eb593b6f12191b
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-03-29 21:14:16 -0300

    Changing linter

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
-import { App } from "./App";
+import { App } from './App';
 import { AppProvider } from "./AppProvider";
 
 dotenv.config();
