6415a3e7ec9b11c089d08c06d6dd11b16fde4f60
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-10 15:57:12 -0300

    Updates k8s and deletes route default handling

--- src/routes/AppRouter.ts
+++ src/routes/AppRouter.ts
@@ -1,7 +1,6 @@
 import { Express } from "express";
 import { UserRouter } from "./UserRouter";
 import { HealthCheckRouter } from "./HealthCheckRouter";
-import express, { Request, Response } from 'express';
 
 export class AppRouter {
   private healthCheckRouter: HealthCheckRouter;
@@ -15,13 +14,5 @@ export class AppRouter {
   public initRoutes(expressApp: Express) {
     expressApp.use("/health-check", this.healthCheckRouter.getRouter());
     expressApp.use("/api/users", this.userRouter.getRouter());
-    // catch-all route for 404 errors
-    expressApp.use((req: Request, res: Response) => {
-      this.handleNotFound(req, res);
-    });
-  }
-  private handleNotFound(req: Request, res: Response) {
-    res.status(404).send("Endpoint not found");
-
   }
 }
