8880678256fb0f73d8e3d43dbb30c8441a2256f7
Tomas Szwarcberg <tomas.szwarcberg@innovid.com>
2023-04-11 01:37:07 -0300

    Adds new methods for getting user metadata in dals

--- src/dal/IUserDal.ts
+++ src/dal/IUserDal.ts
@@ -1,8 +1,19 @@
 import { User } from "@prisma/client";
 
+export interface UserMetadata {
+  userId: number;
+  weight?: number;
+  height?: number;
+  birthDate?: Date;
+  latitude: number;
+  longitude: number;
+}
+
 export interface IUserDal {
   findAll(): Promise<User[]>;
   findById(userId: number): Promise<User | null>;
   findByName(name: string): Promise<User | null>;
   create(name: string, email: string): Promise<User>;
+  addData(data: UserMetadata): Promise<void>;
+  getData(userId: number): Promise<UserMetadata | null>;
 }
--- src/dal/UserDal.ts
+++ src/dal/UserDal.ts
@@ -1,6 +1,6 @@
 import { PrismaClient, User } from "@prisma/client";
 import { ACTIVE_USER } from "../constants/userStateConstants";
-import { IUserDal } from "./IUserDal";
+import { IUserDal, UserMetadata } from "./IUserDal";
 
 export class UserDal implements IUserDal {
   private prismaClient: PrismaClient;
@@ -35,5 +35,37 @@ export class UserDal implements IUserDal {
         state: ACTIVE_USER,
       },
     });
+  }
+
+  public async addData(userMetadata: UserMetadata): Promise<void> {
+    await this.prismaClient.userMetadata.create({
+      data: {
+        userId: userMetadata.userId,
+        weight: userMetadata.weight,
+        height: userMetadata.height,
+        birthDate: userMetadata.birthDate,
+        latitude: userMetadata.latitude,
+        longitude: userMetadata.longitude,
+      },
+    });
+  }
+
+  public async getData(userId: number): Promise<UserMetadata | null> {
+    const userMetadata = await this.prismaClient.userMetadata.findUnique({
+      where: { userId: userId },
+    });
+
+    if (!userMetadata) {
+      return null;
+    }
+
+    return {
+      userId: userMetadata.userId,
+      weight: userMetadata.weight || undefined,
+      height: userMetadata.height || undefined,
+      birthDate: userMetadata.birthDate || undefined,
+      latitude: userMetadata.latitude,
+      longitude: userMetadata.longitude,
+    };
   }
 }
