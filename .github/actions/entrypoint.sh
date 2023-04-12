4e7e706f94accc7b5fa32eb1946faeac4e29b079
Luciano <luckytrujillo123@gmail.com>
2023-04-12 20:03:47 -0300

    fix

--- .github/actions/entrypoint.sh
+++ .github/actions/entrypoint.sh
@@ -21,9 +21,7 @@ okteto build $params
 
 echo build completed successfully
 
-ls
-
 # replace the word latest with the tag of the image in the /kubernetes/deployment.yml file
-sed -i "" "s|latest|$tag|g" ./kubernetes/deployment.yml
+sed -i "s|latest|$tag|g" ./kubernetes/deployment.yml
 
 okteto deploy -f okteto.yml
