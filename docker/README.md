ac7b20bf554e316fd847d7f15483527a6c76ad97
Luciano trujillo <luciano.trujillo@innovid.com>
2023-05-12 18:00:42 -0300

    improved db management and protect creating trainings from non existant trainers

--- docker/README.md
+++
@@ -1,1 +0,0 @@
-This file contains scripts _that should be used in the root of the project_ related to docker commands, with its explanations
--- docker/run_container.sh
+++
@@ -1,6 +0,0 @@
-# This runs the docker container with the image user-service
-# -it is used to run the container in interactive mode
-# -d is used to run the container in detached mode (in the background)
-# -p is used to map the port 7878 of the container to the port 7878 of the host
-# --name is used to name the container user-service (the second user-service is the name of the image)
-docker run -it -d -p 7878:7878 --name user-service user-service
