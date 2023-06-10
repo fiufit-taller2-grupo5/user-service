ea8dcad0ad415f7fd41a51f7349e56eccead2a71
LucianoTrujillo <luciano.trujillo@innovid.com>
2023-06-10 11:56:49 -0300

    todo: add fields to training when creating

--- src/constants/httpConstants.ts
+++ src/constants/httpConstants.ts
@@ -6,4 +6,4 @@ export const UNAUTHORIZED_CODE = 401;
 export const NOT_FOUND_CODE = 404;
 export const CONFLICT_CODE = 409;
 export const INTERNAL_SERVER_ERROR_CODE = 500;
-export const INCORRECT_ROLE_CODE = 403;
+export const INCORRECT_ROLE_CODE = 401;
