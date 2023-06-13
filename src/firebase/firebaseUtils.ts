87efe932e41f384e35e2a4ca66a3671989ec3bd9
merivazquezn <mvazquezn@fi.uba.ar>
2023-06-13 16:15:38 -0300

    response in json

--- src/firebase/firebaseUtils.ts
+++ src/firebase/firebaseUtils.ts
@@ -1,7 +1,5 @@
 import { sendPasswordResetEmail, getAuth } from "firebase/auth";
 import firebaseApp from "./app-config";
-// import storage from "./storage-config";
-// import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export const sendResetPasswordEmail = async (email: string): Promise<boolean> => {
   try {
@@ -15,16 +13,3 @@ export const sendResetPasswordEmail = async (email: string): Promise<boolean> =>
   }
 }
 
-
-// export const uploadFileToFirebaseStorage = async (file: any, path: string): Promise<string> => {
-//   try {
-//     const storageRef = ref(storage, path);
-//     const snapshot = await uploadBytes(storageRef, file);
-//     const downloadURL = await getDownloadURL(snapshot.ref);
-//     console.log("Successfully uploaded file to: ", downloadURL);
-//     return downloadURL;
-//   } catch (err) {
-//     console.log("Failed uploading file: ", err);
-//     throw err;
-//   }
-// }
